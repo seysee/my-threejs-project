89177b7d4469e4112c03c12b2a52a2299b67b205
seyma <Seyma.Tunc@etu.univ-grenoble-alpes.fr>
2025-01-28 18:38:45 +0100

    add: base

--- scripts/world.js
+++ scripts/world.js
@@ -9,20 +9,13 @@ export class World extends THREE.Group{
         this.size = size;
     }
 
-    generate() {
-        for (let x = 0; x < this.size; x++) {
-            for (let z = 0; z < this.size; z++) {
-                const building = this.createBuilding(10, 30, 10, 0x00ff00);
-                building.position.set(x * 12, 15, z * 12);
-                this.add(building);
+    generate(){
+        for (let x = 0; x < this.size; x++){
+            for (let z = 0; z < this.size; z++){
+                const block = new THREE.Mesh(geometry, material);
+                block.position.set(x, 0, z);
+                this.add(block);
             }
         }
-    }
-
-    createBuilding(width, height, depth, color) {
-        const geometry = new THREE.BoxGeometry(width, height, depth);
-        const material = new THREE.MeshLambertMaterial({ color: color });
-        const building = new THREE.Mesh(geometry, material);
-        return building;
     }
 }
